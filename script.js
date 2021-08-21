d1864365ec4d664989668e11a0c05288e9c528f2
Vladi <vladi.villeb@gmail.com>
2021-08-20 20:56:06 -0600

    Subiendo Proyecto-1 Landing Page Tipo Portafolio-CV

--- script.js
+++ script.js
@@ -18,16 +18,16 @@
   });
 })(document);
 
-/* ********** ContactForm ********** */
+/* ********** Contact Form ********** */
+
 ((d) => {
   const $form = d.querySelector(".contact-form"),
     $loader = d.querySelector(".contact-form-loader"),
     $response = d.querySelector(".contact-form-response");
-
   $form.addEventListener("submit", (e) => {
     e.preventDefault();
     $loader.classList.remove("none");
-    fetch("https://formsubmit.co/ajax/your@email.com", {
+    fetch("https://formsubmit.co/ajax/vladi.villeb@gmail.com", {
       method: "POST",
       body: new FormData(e.target),
     })
@@ -40,16 +40,15 @@
       .catch((err) => {
         console.log(err);
         let message =
-          err.statusText || "Ocurrió un error al enviar, intenta nuevamente";
+          err.statusText || "Ocurió un error al enviar, intenta nuevamente";
         $response.querySelector(
           "h3"
-        ).innerHTML = `Error ${err.status}: ${message}`; /* ``  se saca con tecla alt + 96*/
-        $loader.classList.remove("none");
+        ).innerHTML = `Error ${err.status}:${message}`; /* ``  se saca con tecla alt + 96*/
       })
       .finally(() => {
         $loader.classList.add("none");
         setTimeout(() => {
-          location.hash = "#close";
+          location.hash = "close";
         }, 3000);
       });
   });
